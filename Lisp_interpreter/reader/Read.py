31ccd2a665b8c66793d83340b7bff3e185bef9e7
zeina migeed <migeed.z@outlook.com>
2016-06-08 22:36:15 -0400

    Add union types

--- Lisp_interpreter/reader/Read.py
+++ Lisp_interpreter/reader/Read.py
@@ -5,6 +5,45 @@ from ParserError import ParserError
 from Global_Scope import foo
 from Reader import Reader
 from retic import Void, Bool, List, String, Dyn
+# from reader.p_expr import P_Expr
+
+
+#If this class is in a different file, there'd be a bug.
+class P_Expr:
+    """
+    Represents a union type p-expr
+    """
+    def __init__(self, expr):
+        """
+        :param p_expr: List of string or None
+        :return: None
+        """
+        self.expr = expr
+
+class No_Expr(P_Expr):
+    def __init__(self, expr:Void):
+        super().__init__(expr)
+
+class False_Expr(P_Expr):
+    def __init__(self, expr:Bool):
+        super().__init__(expr)
+
+class Expr(P_Expr):
+    def __init__(self, expr:List(String)):
+        super().__init__(expr)
+
+
+
+class Line:
+    """
+    To represent the read line or False if no line was read
+    """
+    def __init__(self, line):
+        """
+        :param line: [String, ...] or False
+        """
+        self.line = line
+
 
 
 def read_eval_print_loop()->Void:
@@ -17,8 +56,7 @@ def read_eval_print_loop()->Void:
     while True:
 
         try:
-            p_expr = read()
-            # print p_expr
+            p_expr = read().expr
             if p_expr == False:
                 break
             elif not p_expr:
@@ -29,25 +67,25 @@ def read_eval_print_loop()->Void:
             if the_value:
                 print (str(the_value))
         except ParserError:
-            print ('bla bla bla')
+            print('bla bla bla')
 
 
-def read()->Dyn:
+def read()->P_Expr:
     """
     Reads a line from the IP and instantiates a Reader using the IP
     and returns the resulting p-expression
     :return: p_expr
     """
     try:
-        ip = read_lines()
+        ip = read_lines().line
         if not ip:
-            return False
+            return False_Expr(False)
         r = Reader(ip)
         p_expr = r.reader()
-        return p_expr
+        return Expr(p_expr)
 
     except:
-        return None
+        return No_Expr(None)
 
 
 def read_lines()->Dyn:
@@ -65,17 +103,17 @@ def read_lines()->Dyn:
             input_list.append(userinput)
             all_input = ' '.join(input_list)
             if has_equal_parens(all_input):
-                return all_input
+                return Line(all_input)
 
         except (EOFError):
-            return None
+            return Line(None)
 
 
 def has_equal_parens(line:String)->Bool:
     """
     Returns True if number of right parens is equal to the number of left parens, and returns False otherwise
     :param line: String
-    :return:
+    :return: Bool
     """
     left = 0
     right = 0
