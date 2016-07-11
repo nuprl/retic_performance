5b867a7dc5bd405b36fe7cce87bf8d18d65844c0
zeina migeed <migeed.z@outlook.com>
2016-07-11 12:13:24 -0400

    add @counter decorator to all functions

--- Espionage/typed/union_find.py
+++ Espionage/typed/union_find.py
@@ -1,19 +1,25 @@
 from retic import fields, Dict, Int, Void, Tuple
+from benchmark_tools.Counter import counted
 
 @fields({'my_dict': Dict(Int, Tuple(Int, Int))})
 class UnionFind:
     my_dict = {}
+
+    @counted
     def __init__(self:UnionFind, my_dict:Dict(Int, Tuple(Int, Int)))->Void:
         self.my_dict = my_dict
 
+    @counted
     def add_node(self:UnionFind, n:Int)->Void:
         self.my_dict[n] = (n, 0)
 
+    @counted
     def find(self:UnionFind, n:Int)->Tuple(Int, Int):
         if self.my_dict[n][0] != n:
             self.my_dict[n] = self.find(self.my_dict[n][0])
         return self.my_dict[n]
 
+    @counted
     def union(self:UnionFind, l1:Tuple(Int, Int), l2:Tuple(Int, Int))->Void:
         k1 = l1[0]
         k2 = l2[0]
