6214f318ebc867165309b3904e87c49f8e0b488f
Ben Greenman <benjaminlgreenman@gmail.com>
2016-10-12 16:56:38 -0400

    convert http2 to ascii
    Remove all unicode characters from the http2 benchmark.
    This makes it less interesting, but I guess that's fine.

--- http2/typed/main.py
+++ http2/typed/main.py
@@ -21,19 +21,19 @@ def main()->Void:
         if not (uri == iri2uri(uri)):
             raise AssertionError("test 1")
     ### 2. test correctness on variant iri
-    if not("http://xn--o3h.com/%E2%98%84" == iri2uri("http://\N{COMET}.com/\N{COMET}")):
+    if not("http://Not-a-COMET.com/Not-a-COMET" == iri2uri("http://Not-a-COMET.com/Not-a-COMET")):
         raise AssertionError("test 2")
-    if not("http://bitworking.org/?fred=%E2%98%84" == iri2uri("http://bitworking.org/?fred=\N{COMET}")):
+    if not("http://bitworking.org/?fred=another non_COMET" == iri2uri("http://bitworking.org/?fred=another non_COMET")):
         raise AssertionError("test 3")
-    if not("http://bitworking.org/#%E2%98%84" == iri2uri("http://bitworking.org/#\N{COMET}")):
+    if not("http://bitworking.org/whats\"with\"all the COMET" == iri2uri("http://bitworking.org/whats\"with\"all the COMET")):
         raise AssertionError("test 4")
-    if not("#%E2%98%84" == iri2uri("#\N{COMET}")):
+    if not("#acOMET" == iri2uri("#acOMET")):
         raise AssertionError("test 5")
-    if not("/fred?bar=%E2%98%9A#%E2%98%84" == iri2uri("/fred?bar=\N{BLACK LEFT POINTING INDEX}#\N{COMET}")):
+    if not("/fred?bar=-BLACK LEFT POINTING INDEX#and-of-course-a-COMET" == iri2uri("/fred?bar=-BLACK LEFT POINTING INDEX#and-of-course-a-COMET")):
         raise AssertionError("test 6")
-    if not("/fred?bar=%E2%98%9A#%E2%98%84" == iri2uri(iri2uri("/fred?bar=\N{BLACK LEFT POINTING INDEX}#\N{COMET}"))):
+    if not("/fred?bar=a#ynotCOMET" == iri2uri(iri2uri("/fred?bar=a#ynotCOMET"))):
         raise AssertionError("test 7")
-    #assertNotEqual("/fred?bar=%E2%98%9A#%E2%98%84", iri2uri("/fred?bar=\N{BLACK LEFT POINTING INDEX}#\N{COMET}".encode('utf-8')))
+    #assertNotEqual("/fred?bar=%E2%98%9A#%E2%98%84", iri2uri("/fred?bar=\N{BLACK LEFT POINTING INDEX}#COMET".encode('utf-8')))
     ### 3. stress test
     iri2uri = Iri2Uri().iri2uri
     testfile = os.path.join(os.path.dirname(__file__), "sample_urls.csv")
@@ -47,3 +47,4 @@ t = Timer()
 with t:
     for i in range(10):
         main()
+
