4a2fb07040217c462876ff3af9922a7af9bd18a3
migeed-z <migeed.z@outlook.com>
2016-10-20 12:22:02 -0400

    ...

--- data/karst/add_types.py
+++ data/karst/add_types.py
@@ -8,13 +8,14 @@ def add_types(file_name, dir_path):
     :return: text file
     """
     list_of_list_lines = parse_file(file_name)
+    file_names = get_file_names(dir_path)
+    max_configs = get_max_configs_all_files(file_names)
     for line_list in list_of_list_lines:
-        file_names = get_file_names(dir_path)
-        max_configs = get_max_configs_all_files(file_names)
         config = line_list[0]
-        nums = config.split("-")
-        print(nums)
+        nums_str = config.split("-")
+        nums = [int(n) for n in nums_str]
         num_types = count_types(nums, max_configs)
+        print(num_types)
         line_list[1] = num_types
 
     new_file = open("%s_2" % file_name, "w")
@@ -37,5 +38,5 @@ def parse_file(file_name):
     return res
 
 
-add_types('Espionage.txt', '/../../Espionage')
+add_types('Espionage.txt', '/Users/zeinamigeed/Documents/fall2016/software_dev/retic_performance/Espionage')
 
