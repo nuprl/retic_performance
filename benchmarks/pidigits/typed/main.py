ab2af51f7b6d1c8a3eac03daf92fb8e9b4c75e3a
Ben Greenman <benjaminlgreenman@gmail.com>
2017-03-19 17:02:33 -0400

    [benchmark] move pidigits 'def's to top-level
    Because the script for generating configurations does not remove types from
    nested `def` statements.

--- benchmarks/pidigits/typed/main.py
+++ benchmarks/pidigits/typed/main.py
@@ -15,37 +15,37 @@ import itertools
 
 NDIGITS = 5000
 
-def calc_ndigits(n:int)->List(int):
-    # Adapted from code on http://shootout.alioth.debian.org/
-    def gen_x(k:int)->(int,int,int,int):
-        return (k, 4*k + 2, 0, 2*k + 1)
+# Adapted from code on http://shootout.alioth.debian.org/
+def gen_x(k:int)->(int,int,int,int):
+    return (k, 4*k + 2, 0, 2*k + 1)
 
-    def compose(a:(int,int,int,int), b:(int,int,int,int))->(int,int,int,int):
-        aq, ar, as_, at = a
-        bq, br, bs, bt = b
-        return (aq * bq,
-                aq * br + ar * bt,
-                as_ * bq + at * bs,
-                as_ * br + at * bt)
+def compose(a:(int,int,int,int), b:(int,int,int,int))->(int,int,int,int):
+    aq, ar, as_, at = a
+    bq, br, bs, bt = b
+    return (aq * bq,
+            aq * br + ar * bt,
+            as_ * bq + at * bs,
+            as_ * br + at * bt)
 
-    def extract(z:(int,int,int,int), j:int)->int:
-        q, r, s, t = z
-        return (q*j + r) // (s*j + t)
+def extract(z:(int,int,int,int), j:int)->int:
+    q, r, s, t = z
+    return (q*j + r) // (s*j + t)
 
-    def pi_digits(limit:int)->List(int):
-        z = (1, 0, 0, 1)
-        x = 1
-        result = []
-        while (x <= limit):
+def pi_digits(limit:int)->List(int):
+    z = (1, 0, 0, 1)
+    x = 1
+    result = []
+    while (x <= limit):
+        y = extract(z, 3)
+        while y != extract(z, 4):
+            z = compose(z, gen_x(x))
             y = extract(z, 3)
-            while y != extract(z, 4):
-                z = compose(z, gen_x(x))
-                y = extract(z, 3)
-            z = compose((10, -10*y, 0, 1), z)
-            x += 1
-            result.append(y)
-        return result
+        z = compose((10, -10*y, 0, 1), z)
+        x += 1
+        result.append(y)
+    return result
 
+def calc_ndigits(n:int)->List(int):
     return pi_digits(n)
 
 if __name__ == "__main__":
