02f6d561d24b782edaa75b554666432d24bbfc4f
Ben Greenman <benjaminlgreenman@gmail.com>
2017-03-27 19:04:41 -0400

    [dls] add 'raco rp-info <benchmark-name>'
    Print static information about the given benchmarks
     in an un-specified output format.

--- dls/info.rkt
+++ dls/info.rkt
@@ -15,6 +15,7 @@
 (define version "0.1")
 (define pkg-authors '(ben zeina))
 (define raco-commands '(
+  ("rp-info" (submod gm-dls-2017/script/benchmark-info main) "Print static benchmark info" #f)
   ("rp-plot" (submod gm-dls-2017/script/plot main) "Render overhead plot" #f)
   ("rp-perf" (submod gm-dls-2017/script/performance-info main) "Print performance statistics" #f)
   ("rp-sample" (submod gm-dls-2017/script/sample main) "Generate samples for a benchmark" #f)
