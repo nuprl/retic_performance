581e8d9932a2ce6dc3a2be6b15ce276f988d66ba
Ben Greenman <benjaminlgreenman@gmail.com>
2017-03-29 01:14:58 -0400

    [dls] save copy of overhead plots figure

--- dls/script/render.rkt
+++ dls/script/render.rkt
@@ -36,27 +36,29 @@
 (define (render-exact-runtime-plot* bm*)
   (render-benchmark* bm* "exact-runtime" exact-runtime-plot))
 
+;; TODO try checkpointing each picture
 (define (render-benchmark* bm* descr render-one)
   (define bm-name* (map benchmark->name bm*))
-  (with-cache (cachefile (format "~a-plot.rktd" descr))
-    #:keys (list (λ () (list* OVERHEADS-WIDTH OVERHEADS-HEIGHT OVERHEADS-VSPACE OVERHEADS-HSPACE bm-name*)))
-    (λ ()
-      (define pi*
-        (for/list ([bm (in-list bm*)])
-          ;; TODO don't hide errors
-          (with-handlers ([exn:fail? (λ (e) (printf "No performance-info for ~a, skipping" (benchmark->name bm)))])
-            (benchmark->performance-info bm))))
-      (define num-pict (length pi*))
-      (define p**
-        (parameterize ([*OVERHEAD-PLOT-WIDTH* (exact-floor (/ (- OVERHEADS-WIDTH OVERHEADS-HSPACE) NUM-COLUMNS))]
-                       [*OVERHEAD-PLOT-HEIGHT* (exact-floor (/ (+ OVERHEADS-HEIGHT OVERHEADS-VSPACE) num-pict))]
-                       [*OVERHEAD-SHOW-RATIO* #f]
-                       [*LEGEND-VSPACE* 2]
-                       [*FONT-SIZE* 8])
-          (columnize (map render-one pi*) NUM-COLUMNS)))
-      (define col*
-        (map (λ (p*) (apply vl-append OVERHEADS-VSPACE p*)) p**))
-      (apply ht-append OVERHEADS-HSPACE col*))))
+  (parameterize ([*current-cache-directory* (build-path (current-directory) "with-cache")])
+    (with-cache (cachefile (format "~a-plot.rktd" descr))
+      #:keys (list (λ () (list* OVERHEADS-WIDTH OVERHEADS-HEIGHT OVERHEADS-VSPACE OVERHEADS-HSPACE bm-name*)))
+      (λ ()
+        (define pi*
+          (for/list ([bm (in-list bm*)])
+            ;; TODO don't hide errors
+            (with-handlers ([exn:fail? (λ (e) (printf "No performance-info for ~a, skipping" (benchmark->name bm)))])
+              (benchmark->performance-info bm))))
+        (define num-pict (length pi*))
+        (define p**
+          (parameterize ([*OVERHEAD-PLOT-WIDTH* (exact-floor (/ (- OVERHEADS-WIDTH OVERHEADS-HSPACE) NUM-COLUMNS))]
+                         [*OVERHEAD-PLOT-HEIGHT* (exact-floor (/ (+ OVERHEADS-HEIGHT OVERHEADS-VSPACE) num-pict))]
+                         [*OVERHEAD-SHOW-RATIO* #f]
+                         [*LEGEND-VSPACE* 2]
+                         [*FONT-SIZE* 8])
+            (columnize (map render-one pi*) NUM-COLUMNS)))
+        (define col*
+          (map (λ (p*) (apply vl-append OVERHEADS-VSPACE p*)) p**))
+        (apply ht-append OVERHEADS-HSPACE col*)))))
 
 ;; =============================================================================
 
