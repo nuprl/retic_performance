3d10db4db0b5e7791da38d8fb5418dac78eaef3a
Ben Greenman <benjaminlgreenman@gmail.com>
2017-03-31 11:53:11 -0400

    [dls] use & test openssl library's MD5 function

--- dls/script/system.rkt
+++ dls/script/system.rkt
@@ -6,15 +6,22 @@
 (provide
   (contract-out
    [shell
-    (-> string? (listof string?) string?)]
+    (-> string? (or/c string? (listof string?)) string?)]
    ;; `(shell cmd arg*)` finds the executable that `cmd` denotes,
    ;;  then invokes the executable with arguments `arg*`.
    ;; Raises an exception if the executable exists uncleanly,
    ;;  otherwise returns a string containing all output produced by the exe.
+
+   [md5sum
+    (-> path-string? string?)]
+   ;; Compute MD5 hash of the contents of the given file
+
 ))
 
 (require
   "util.rkt"
+  (only-in openssl/md5
+    md5)
   (only-in racket/port
     with-output-to-string)
   (only-in racket/string
@@ -26,9 +33,10 @@
 
 ;; TODO warn if falling back?
 
-(define (shell pre-exe cmd*)
+(define (shell pre-exe pre-cmd)
   (define exe (find-exe pre-exe))
   (define success? (box #f))
+  (define cmd* (if (string? pre-cmd) (list pre-cmd) pre-cmd))
   (define str
     (with-output-to-string
       (lambda ()
@@ -44,10 +52,14 @@
     fep
     (raise-user-error 'shell "cannot find executable '~a', please install and try again" pre-exe)))
 
+(define (md5sum ps)
+  (call-with-input-file ps md5))
+
 ;; =============================================================================
 
 (module+ test
-  (require rackunit)
+  (require rackunit racket/runtime-path (only-in racket/string string-split))
+  (define-runtime-path sloc-example "test/sloc-example.py")
 
   (test-case "shell"
     (check-regexp-match #rx"^Welcome to Racket"
@@ -60,4 +72,12 @@
     (check-exn exn:fail:user?
       (lambda () (find-exe "this-is-not-racket-this-is-sparta"))))
 
+  (test-case "md5sum"
+    (define (check-md5 ps)
+      (define openssl-md5 (md5sum sloc-example))
+      (define system-md5 (car (string-split (shell "md5sum" (path->string sloc-example)))))
+      (check-equal? openssl-md5 system-md5))
+
+    (check-md5 sloc-example)
+  )
 )
